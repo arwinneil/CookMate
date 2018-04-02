98cd38aba97cf8543c8beb71a3c58dfd219ad3c8
Arwin Neil Baichoo <arwinneil@gmail.com>
2018-04-03 00:06:41 +0400

    Added Detail Page

---
+++ CookMate/CookMate/DetailPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CookMate
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailPage : ContentPage
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Pressed(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage()
+            {
+                Title = "Spaghetti",
+            });
+        }
+    }
+}

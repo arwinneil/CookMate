f4d2a687bcb6d6e67585abf8fd629c843eed5d67
Arwin Neil Baichoo <arwinneil@gmail.com>
2018-04-11 20:09:18 +0400

    Update Data

--- CookMate/CookMate/Data.cs
+++ CookMate/CookMate/Data.cs
@@ -5,42 +5,94 @@ public partial class Data
 {
     public static List<Recipe> recipes = new List<Recipe>()
         {
-
+            //***************************************************  Recipe 1 *********************************************
             new Recipe("Spaghetti", new List<string>
-            { "Step 1 Bring a pot of salted water to a boil, add spaghetti and cook according to package directions, stirring often, until al dente. Drain pasta and rinse well with cold water.",
-                "Step 2 Preheat oven to 400°F. Grease an ovenproof 9- or 10-inch skillet with about 1/2 Tbsp. butter.",
-                "Step 3 In a bowl, whisk together eggs, milk, bacon, scallion, pepper and 3 Tbsp. Parmesan. Add spaghetti; mix well.Transfer mixture to skillet, spreading evenly. Dot with remaining butter and sprinkle remaining 1 Tbsp.Parmesan on top.Bake for 25 to 30 minutes, until center is set and top is golden.Let cool for 10 minutes before serving."
-            },
 
-                30,
+            //Ingredients
 
-                new List<string>
-            { "Step 1 Bring a pot of salted water to a boil, add spaghetti and cook according to package directions, stirring often, until al dente. Drain pasta and rinse well with cold water.",
-                "Step 2 Preheat oven to 400°F. Grease an ovenproof 9- or 10-inch skillet with about 1/2 Tbsp. butter.",
-                "Step 3 In a bowl, whisk together eggs, milk, bacon, scallion, pepper and 3 Tbsp. Parmesan. Add spaghetti; mix well.Transfer mixture to skillet, spreading evenly. Dot with remaining butter and sprinkle remaining 1 Tbsp.Parmesan on top.Bake for 25 to 30 minutes, until center is set and top is golden.Let cool for 10 minutes before serving."
-            })
+                { "1.4kg Red Delight potatoes, peeled",
+                    "3 fresh rosemary sprigs, leaves picked",
+                    "250ml (1 cup) thickened cream",
+                    "35g butter, finely chopped"
+                },
 
-            ,
+            //Time
+                    30,
+
+            //Steps
+                 new List<string>
+                { "Step 1 Bring a pot of salted water to a boil, add spaghetti and cook according to package directions, stirring often, until al dente. Drain pasta and rinse well with cold water.",
+                    "Step 2 Preheat oven to 400°F. Grease an ovenproof 9- or 10-inch skillet with about 1/2 Tbsp. butter.",
+                    "Step 3 In a bowl, whisk together eggs, milk, bacon, scallion, pepper and 3 Tbsp. Parmesan. Add spaghetti; mix well.Transfer mixture to skillet, spreading evenly. Dot with remaining butter and sprinkle remaining 1 Tbsp.Parmesan on top.Bake for 25 to 30 minutes, until center is set and top is golden.Let cool for 10 minutes before serving."
+                }),
+
+            //***************************************************  Recipe 2  *********************************************
 
             new Recipe("Cheesy corn and tuna balls", new List<string>
-            { "Step 1 Bring a pot of salted water to a boil, add spaghetti and cook according to package directions, stirring often, until al dente. Drain pasta and rinse well with cold water.",
-                "Step 2 Preheat oven to 400°F. Grease an ovenproof 9- or 10-inch skillet with about 1/2 Tbsp. butter.",
-                "Step 3 In a bowl, whisk together eggs, milk, bacon, scallion, pepper and 3 Tbsp. Parmesan. Add spaghetti; mix well.Transfer mixture to skillet, spreading evenly. Dot with remaining butter and sprinkle remaining 1 Tbsp.Parmesan on top.Bake for 25 to 30 minutes, until center is set and top is golden.Let cool for 10 minutes before serving."
-            },
 
-            30,
+            //Ingredients
 
-                new List<string>
-            { "Step 1 Bring a pot of salted water to a boil, add spaghetti and cook according to package directions, stirring often, until al dente. Drain pasta and rinse well with cold water.",
-                "Step 2 Preheat oven to 400°F. Grease an ovenproof 9- or 10-inch skillet with about 1/2 Tbsp. butter.",
-                "Step 3 In a bowl, whisk together eggs, milk, bacon, scallion, pepper and 3 Tbsp. Parmesan. Add spaghetti; mix well.Transfer mixture to skillet, spreading evenly. Dot with remaining butter and sprinkle remaining 1 Tbsp.Parmesan on top.Bake for 25 to 30 minutes, until center is set and top is golden.Let cool for 10 minutes before serving."
-            })
+                { "600g sebago potatoes, peeled, chopped",
+                  "185g can tuna in springwater, drained, flaked",
+                  "125g can corn kernels, drained",
+                  "2 tablespoons chopped fresh chives",
+                },
 
+            //Time
+                    30,
 
-        };
+            //Steps
+                 new List<string>
+                {   "Step 1 Cook the potato in a saucepan of boiling water for 15 minutes or until tender enough to mash. Drain well. Transfer to a heatproof bowl and set aside to cool slightly.",
+                    "Step 2 Use a potato masher to coarsely mash the potato. Add the tuna, corn, chives and cheese and stir until combined. Roll level tablespoonfuls of the mixture into balls. Place on a tray lined with baking paper.",
+                    "Step 3 Place the flour, egg and cornflake crumbs in separate shallow bowls. Roll the balls in the flour, dip in egg and then roll in cornflake crumbs to coat. Return to the prepared tray.",
+                    "Step 4 Add enough oil to a large frying pan to reach a depth of 1cm. Heat over medium-high heat. Working in batches, shallow-fry the balls, turning often, for 5 minutes or until crisp and golden brown. Transfer to a plate lined with paper towel to drain. Serve with sweet chilli sauce, if you like."
+                }),
 
-    
+             //***************************************************  Recipe 3  *********************************************
 
+            new Recipe("Potato Pave", new List<string>
 
-    
+            //Ingredients
+
+                { "1.4kg Red Delight potatoes, peeled",
+                  "3 fresh rosemary sprigs, leaves picked",
+                  "250ml (1 cup) thickened cream",
+                  "35g butter, finely chopped"
+                },
+
+            //Time
+                    30,
+
+            //Steps
+                 new List<string>
+                {   "Step 1 Preheat oven to 180C. Line the base and 2 long sides of a 22 x 11cm (base measurement) loaf pan with baking paper, allowing the sides to overhang.",
+                    "Step 2 Use a mandolin or a sharp knife to thinly slice the potatoes. Finely chop the rosemary leaves. Place 2 layers of potato in the prepared tin. Top with a little cream, butter and rosemary. Season with salt. Continue layering with remaining potato, cream, butter and rosemary, finishing with potato.",
+                    "Step 3 Fold over the baking paper and cover with foil. Bake for 1 hour or until the potato is tender. Uncover and cook for a further 30 minutes or until golden. Set aside to cool. Fold over the baking paper. Place a heavy object (such as 2 or 3 cans) on top to compress. Place in the fridge overnight to set.",
+                    "Step 4 Preheat oven to 200C. Line a baking tray with baking paper. Turn the potato stack onto a chopping board. Trim the edges and cut into 6 even slices. Place the potato slices, cut-side up, on the prepared tray. Bake for 10 minutes or until light golden. Turn and cook for a further 10-15 minutes or until golden."
+                }),
+
+             //***************************************************  Recipe 4  *********************************************
+
+            new Recipe("Spicy Dholl with eggs", new List<string>
+
+            //Ingredients
+
+                {   "2 tablespoons olive oil",
+                    "1 onion, finely chopped",
+                    "1/4 cup fresh curry leaves",
+                    "2 garlic cloves, crushed",
+                },
+
+            //Time
+                    30,
+
+            //Steps
+                 new List<string>
+                {   "Step 1 Heat 1 tbs oil in a large saucepan. Add the onion and cook, stirring, for 2-3 minutes. Reserve a few of the curry leaves for the garnish. Add the rest of the curry leaves to the pan and cook for 1 minute. Add garlic and cumin and cook, stirring, for a further minute.",
+                    "Step 2 Add the lentils and stir until well coated. Pour in the stock and bring the lentil mixture to the boil. Reduce the heat and simmer for 15 minutes or until the lentils are soft. Add the spinach and cook, stirring, until just wilted. Season",
+                    "Step 3 Meanwhile, spray a large non-stick frying pan with oil. Place over medium heat. Fry the eggs for 2-3 minutes for sunny-side up.",
+                    "Step 4 Divide the dhal among serving bowls. Top with fried eggs. Drizzle with the remaining oil. Scatter over the tomato, coriander and reserved curry leaves. Serve with pita bread and yoghurt, if you like."
+                }),
+           };
 }

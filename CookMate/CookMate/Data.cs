55023889c8c79b2014ea4c7eaa8c389d030331db
Arwin Neil Baichoo <arwinneil@gmail.com>
2018-04-11 09:26:41 +0400

    Added Recipe Classes

---
+++ CookMate/CookMate/Data.cs
@@ -0,0 +1,46 @@
+﻿using CookMate;
+using System.Collections.Generic;
+
+public partial class Data
+{
+    public static List<Recipe> recipes = new List<Recipe>()
+        {
+
+            new Recipe("Spaghetti", new List<string>
+            { "Step 1 Bring a pot of salted water to a boil, add spaghetti and cook according to package directions, stirring often, until al dente. Drain pasta and rinse well with cold water.",
+                "Step 2 Preheat oven to 400°F. Grease an ovenproof 9- or 10-inch skillet with about 1/2 Tbsp. butter.",
+                "Step 3 In a bowl, whisk together eggs, milk, bacon, scallion, pepper and 3 Tbsp. Parmesan. Add spaghetti; mix well.Transfer mixture to skillet, spreading evenly. Dot with remaining butter and sprinkle remaining 1 Tbsp.Parmesan on top.Bake for 25 to 30 minutes, until center is set and top is golden.Let cool for 10 minutes before serving."
+            },
+
+                30,
+
+                new List<string>
+            { "Step 1 Bring a pot of salted water to a boil, add spaghetti and cook according to package directions, stirring often, until al dente. Drain pasta and rinse well with cold water.",
+                "Step 2 Preheat oven to 400°F. Grease an ovenproof 9- or 10-inch skillet with about 1/2 Tbsp. butter.",
+                "Step 3 In a bowl, whisk together eggs, milk, bacon, scallion, pepper and 3 Tbsp. Parmesan. Add spaghetti; mix well.Transfer mixture to skillet, spreading evenly. Dot with remaining butter and sprinkle remaining 1 Tbsp.Parmesan on top.Bake for 25 to 30 minutes, until center is set and top is golden.Let cool for 10 minutes before serving."
+            })
+
+            ,
+
+            new Recipe("Cheesy corn and tuna balls", new List<string>
+            { "Step 1 Bring a pot of salted water to a boil, add spaghetti and cook according to package directions, stirring often, until al dente. Drain pasta and rinse well with cold water.",
+                "Step 2 Preheat oven to 400°F. Grease an ovenproof 9- or 10-inch skillet with about 1/2 Tbsp. butter.",
+                "Step 3 In a bowl, whisk together eggs, milk, bacon, scallion, pepper and 3 Tbsp. Parmesan. Add spaghetti; mix well.Transfer mixture to skillet, spreading evenly. Dot with remaining butter and sprinkle remaining 1 Tbsp.Parmesan on top.Bake for 25 to 30 minutes, until center is set and top is golden.Let cool for 10 minutes before serving."
+            },
+
+            30,
+
+                new List<string>
+            { "Step 1 Bring a pot of salted water to a boil, add spaghetti and cook according to package directions, stirring often, until al dente. Drain pasta and rinse well with cold water.",
+                "Step 2 Preheat oven to 400°F. Grease an ovenproof 9- or 10-inch skillet with about 1/2 Tbsp. butter.",
+                "Step 3 In a bowl, whisk together eggs, milk, bacon, scallion, pepper and 3 Tbsp. Parmesan. Add spaghetti; mix well.Transfer mixture to skillet, spreading evenly. Dot with remaining butter and sprinkle remaining 1 Tbsp.Parmesan on top.Bake for 25 to 30 minutes, until center is set and top is golden.Let cool for 10 minutes before serving."
+            })
+
+
+        };
+
+    
+
+
+    
+}
---
+++ CookMate/CookMate/Recipes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CookMate
+{
+    public class Recipe
+    {
+        public string Name;
+        public List<string> Ingredients;
+        public int Time;
+        public List<string> Steps;
+
+        public Recipe(string name, List<string> ingredients, int time, List<string> steps)
+        {
+            this.Name = name;
+            this.Ingredients = ingredients;
+            this.Time = time;
+            this.Steps = steps;
+        }
+    }
+}

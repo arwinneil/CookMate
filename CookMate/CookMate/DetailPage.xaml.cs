9ccbe60d041b025ae09af5e3e943644671201a7f
Arwin Neil Baichoo <arwinneil@gmail.com>
2018-04-11 09:26:21 +0400

    Update Recipes

--- CookMate/CookMate/DetailPage.xaml.cs
+++ CookMate/CookMate/DetailPage.xaml.cs
@@ -8,16 +8,30 @@ namespace CookMate
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailPage : ContentPage
     {
-        public DetailPage()
+        public DetailPage(string x)
         {
             InitializeComponent();
+            Title = "Recipe Details";
+            picture.Source = "spaget.jpg";
+
+            if (x == "Cheesy Corn and Tuna Balls")
+                picture.Source = "Balls.jpeg";
+
+            if (x == "Spaghetti")
+                picture.Source = "spaghetti.jpeg";
+
+            if (x == "Potato Pave")
+                picture.Source = "potato.jpeg";
+
+            if (x == "Spicy dhal with eggs")
+                picture.Source = "dhalleggs.jpeg";
         }
 
         private void Button_Pressed(object sender, EventArgs e)
         {
             Navigation.PushAsync(new MainPage()
             {
-                Title = "Spaghetti",
+                Title = this.Title
             });
         }
     }
--- CookMate/CookMate/HomePage.xaml.cs
+++ CookMate/CookMate/HomePage.xaml.cs
@@ -14,16 +14,33 @@ namespace CookMate
             {
                 Command = new Command(() =>
                 {
-                    Navigation.PushAsync(new DetailPage()
-                    {
-
-
-                        Title = "Spaghetti",
-
-                    });
+                    Navigation.PushAsync(new DetailPage("Spaghetti"));
                 })
-                
+            });
+
+            Ball.GestureRecognizers.Add(new TapGestureRecognizer()
+            {
+                Command = new Command(() =>
+                {
+                    Navigation.PushAsync(new DetailPage("Cheesy Corn and Tuna Balls"));
+                })
+            });
+
+            Potato.GestureRecognizers.Add(new TapGestureRecognizer()
+            {
+                Command = new Command(() =>
+                {
+                    Navigation.PushAsync(new DetailPage("Potato Pave"));
+                })
+            });
+
+            Dhalleggs.GestureRecognizers.Add(new TapGestureRecognizer()
+            {
+                Command = new Command(() =>
+                {
+                    Navigation.PushAsync(new DetailPage("Spicy dhal with eggs"));
+                })
             });
         }
-	}
+    }
 }

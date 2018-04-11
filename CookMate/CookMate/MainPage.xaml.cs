77f845d7959d081e2d8c7824d4c9a92f7addfaa5
Arwin Neil Baichoo <arwinneil@gmail.com>
2018-04-11 20:48:17 +0400

    Remove redundant buttons

--- CookMate/CookMate/MainPage.xaml.cs
+++ CookMate/CookMate/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Plugin.SpeechRecognition;
+using Plugin.TextToSpeech;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xamarin.Forms;
 
 namespace CookMate
@@ -13,54 +15,65 @@ namespace CookMate
                "Step 3 In a bowl, whisk together eggs, milk, bacon, scallion, pepper and 3 Tbsp. Parmesan. Add spaghetti; mix well.Transfer mixture to skillet, spreading evenly. Dot with remaining butter and sprinkle remaining 1 Tbsp.Parmesan on top.Bake for 25 to 30 minutes, until center is set and top is golden.Let cool for 10 minutes before serving."
             };
 
-
-
-        public MainPage()
+       
+        public MainPage(String name)
         {
             InitializeComponent();
 
-            Listen();
+            Recipe recipe = Data.recipes.Single(r => r.Name == name);
 
-            Carou.ItemsSource = receipe;
-
-     
-        }
-
-        private  void Listen()
-
-        {
             
 
-            var listener = CrossSpeechRecognition
-                    .Current
-                       .ContinuousDictation()
-                     // .ListenUntilPause()
+            Carou.ItemsSource = recipe.Steps;
 
-                    .Subscribe(phrase =>
-                    {
-                        label.Text = phrase;
-                        label.TextColor = Color.Black;
-
-                        if (phrase.IndexOf("next") > -1)
-                        {
-                            label.TextColor = Color.Red;
-
-                            if (Carou.Position < receipe.Count - 1)
-                            {
-                                Carou.Position++;
-                                Carou.ItemsSource = receipe;
-                            }
-                        }
-                        else if (phrase.IndexOf("previous") > -1)
-                        {
-                            if (Carou.Position > 0)
-                            {
-                                Carou.Position--;
-                                Carou.ItemsSource = receipe;
-                            }
-                        }
-                    });
-
+            Listen();
         }
+
+        async public void  Listen()
+
+        {
+            //var listener = CrossSpeechRecognition
+            //        .Current
+            //          .ContinuousDictation()
+            //         // .ListenUntilPause()
+
+            var listener = CrossSpeechRecognition
+            .Current
+           .ContinuousDictation()
+
+            .Subscribe( phrase =>
+            {
+                label.Text = phrase;
+                label.TextColor = Color.Black;
+
+                if (phrase.IndexOf("next") > -1)
+                {
+                    label.TextColor = Color.Red;
+
+                    if (Carou.Position < receipe.Count - 1)
+                    {
+                        Carou.Position++;
+                        Carou.ItemsSource = receipe;
+                    }
+                }
+                else if (phrase.IndexOf("previous") > -1)
+                {
+                    if (Carou.Position > 0)
+                    {
+                        Carou.Position--;
+                        Carou.ItemsSource = receipe;
+                    }
+                }
+                if ((phrase.IndexOf("speak") > -1) || (phrase.IndexOf("read") > -1) || (phrase.IndexOf("tell") > -1))
+                {
+                     CrossTextToSpeech.Current.Speak(receipe[Carou.Position]);
+
+          
+                }
+            });
+
+           
+        }
+
     }
 }

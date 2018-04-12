23b3e13c157efc54cec32e7735e0d8fe4a097969
Arwin Neil Baichoo <arwinneil@gmail.com>
2018-04-12 08:54:57 +0400

    Added time & ingredients

--- CookMate/CookMate/DetailPage.xaml.cs
+++ CookMate/CookMate/DetailPage.xaml.cs
@@ -13,11 +13,11 @@ namespace CookMate
         public DetailPage(string name, string desc)
         {
             InitializeComponent();
-
+            Recipe recipe = Data.recipes.Single(r => r.Name == name);
             _name = name;
 
             Title = "Recipe Details";
-            title.Text = name;
+            title.Text = name +" - " + recipe.Time +"min";
             description.Text = desc;
 
         
@@ -34,7 +34,12 @@ namespace CookMate
             if (name == "Spicy dhal with eggs")
                 picture.Source = "dhalleggs.jpeg";
 
-            Recipe recipe = Data.recipes.Single(r => r.Name == name);
+           
+
+            i1.Text = recipe.Ingredients[0];
+            i2.Text = recipe.Ingredients[1];
+            i3.Text = recipe.Ingredients[2];
+            i4.Text = recipe.Ingredients[3];
         }
 
         private void Button_Pressed(object sender, EventArgs e)
